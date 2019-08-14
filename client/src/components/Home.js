0ac4f34230bbdb79e0ba314461c55d7f3393c319
donchelli <tim.melcherson@gmail.com>
2019-08-14 22:30:21 +0200

    Okay now we are ready to go.

---
+++ client/src/components/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+
+	}
+
+	render() {
+		return (
+			<div>
+				<p>HELLO</p>
+			</div>
+		);
+	}
+}
+
+Home.propTypes = {
+
+};
+
+export default Home;
